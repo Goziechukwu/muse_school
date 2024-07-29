9e013ff2947b0f0aef3d8cce85ee5a25fc6fc8e5
Goziechukwu Inekwe <inekwegoziechukwu@gmail.com>
2024-07-29 03:10:15 +0000

    edit checkout success html

--- checkout/views.py
+++ checkout/views.py
@@ -129,7 +129,8 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
-    product = order.product
+    # Fetch the OrderLineItems related to the order
+    order_line_items = OrderLineItem.objects.filter(order=order)
 
     if request.user.is_authenticated:
         profile = UserProfile.objects.get(user=request.user)
@@ -159,7 +160,7 @@ def checkout_success(request, order_number):
     template = 'checkout/checkout_success.html'
     context = {
         'order': order,
-        'product': product,
+        'order_line_items': order_line_items,
     }
 
     return render(request, template, context)
