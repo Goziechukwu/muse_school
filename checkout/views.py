ba7d08cd2c416d8bfead2a641c97fd7a0bf4f0e5
Goziechukwu Inekwe <inekwegoziechukwu@gmail.com>
2024-07-29 03:36:57 +0000

    import OrderLineItem in views.py

--- checkout/views.py
+++ checkout/views.py
@@ -6,7 +6,7 @@ from django.contrib import messages
 from django.conf import settings
 
 from .forms import OrderForm
-from .models import Order, Product
+from .models import Order, Product, OrderLineItem
 
 from profiles.models import UserProfile
 from profiles.forms import UserProfileForm
@@ -129,8 +129,7 @@ def checkout_success(request, order_number):
     save_info = request.session.get('save_info')
     order = get_object_or_404(Order, order_number=order_number)
 
-    # Fetch the OrderLineItems related to the order
-    order_line_items = OrderLineItem.objects.filter(order=order)
+
 
     if request.user.is_authenticated:
         profile = UserProfile.objects.get(user=request.user)
@@ -159,8 +158,7 @@ def checkout_success(request, order_number):
 
     template = 'checkout/checkout_success.html'
     context = {
-        'order': order,
-        'order_line_items': order_line_items,
+        'order': order
     }
 
     return render(request, template, context)
